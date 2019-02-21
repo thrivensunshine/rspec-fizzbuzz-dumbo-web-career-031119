62cf8d552b7cb6cdea21f847c18dff8d0802871f
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-21 05:06:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,13 @@
 require 'pry'
+<<<<<<< HEAD
 #
+=======
+>>>>>>> 3b78b96432d1e46b756a1c1bc95ce3934e9362eb
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
+<<<<<<< HEAD
 
   if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
@@ -18,3 +22,17 @@ def fizzbuzz(num)
 
   end
 end
+=======
+  binding.pry
+  if num % 3 == 0 && num % 5 == 0 
+    return "FizzBuzz"
+  elsif num % 3 == 0 
+    return "Fizz"
+  elsif num % 5 == 0 
+    return "Buzz"
+  elseif 
+    return nil
+
+  end
+end  
+>>>>>>> 3b78b96432d1e46b756a1c1bc95ce3934e9362eb

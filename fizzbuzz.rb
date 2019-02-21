87fa7a6f1aac67b6acd8be2cbe3914b5da138e3f
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-21 05:07:56 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,8 @@
 require 'pry'
-#
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+
 
 def fizzbuzz(num)
+
 
   if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
